0a1ac4f11192d4cc0449b2712edec82d1f7c852a
Justin MacCallum <justin.maccallum@me.com>
2015-10-06 15:29:56 -0600

    Updated script to exctract unique elements from a column

--- extract.sh
+++ extract.sh
@@ -1,3 +1,5 @@
 # Tiffany Timbers, October 6, 2015
 # This script will extract unique elements from a colum
 # of a csv file
+
+cut -f $1 -d ',' data/antibiotics.csv | sort | uniq > antibiotics_unique.csv
